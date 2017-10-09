e900854904338a8588f30e75f7507d5a5af6e40c
phra <greensoncio@gmail.com>
2017-10-09 15:07:32 +0200

    refactor: update examples

--- examples/index.ts
+++ examples/index.ts
@@ -1,14 +1,24 @@
 /// <reference path="../untyped.d.ts" />
 
+import * as uuid from 'uuid/v1'
+
 import {
   formatDirectoryProtocolXML,
   formatTransactionProtocolXML,
+  formatStatusProtocolXML,
 } from '../lib'
 
+const id = uuid().replace(/-/g, '')
+
 console.log(`
+ID #${id}
+
 DIRECTORY REQUEST
 ${formatDirectoryProtocolXML()}
 
 TRANSACTION REQUEST
-${formatTransactionProtocolXML()}
+${formatTransactionProtocolXML('BANKNL2Y')}
+
+STATUS REQUEST
+${formatStatusProtocolXML(id)}
 `)
