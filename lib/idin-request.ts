b64e960511efcf02ba87b12a11078bc02543d2d2
phra <greensoncio@gmail.com>
2017-10-15 02:03:10 +0200

    feat: add xml parsing of directory response
    fixes #23

--- lib/idin-request.ts
+++ lib/idin-request.ts
@@ -1,4 +1,7 @@
+import { ifError } from 'assert'
+import to from 'await-to-ts'
 import 'isomorphic-fetch'
+import { xml2json } from 'xml-js'
 import { ROUTING_ENDPOINT } from './constants'
 import { formatDirectoryProtocolXML } from './directory-protocol'
 import { formatStatusProtocolXML } from './status-protocol'
@@ -22,6 +25,50 @@ export function fetchResponse(payload?: string, routingService = ROUTING_ENDPOIN
 export function fetchDirectoryResponse() {
   const payload = formatDirectoryProtocolXML()
   return fetchResponse(payload)
+}
+
+// tslint:disable:no-string-literal
+export async function getDirectoryResponse() {
+  const payload = formatDirectoryProtocolXML()
+  const [err, res] = await to(fetchDirectoryResponse())
+  ifError(err)
+  const parsed = JSON.parse(xml2json(res, { compact: true }) as any)
+  return {
+    createDateTimestamp: parsed['ns3:DirectoryRes']['ns3:createDateTimestamp']._text,
+    Acquirer: {
+      acquirerID: parsed['ns3:DirectoryRes']['ns3:Acquirer']['ns3:acquirerID']._text,
+    },
+    Directory: {
+      directoryDateTimestamp: parsed['ns3:DirectoryRes']['ns3:Directory']['ns3:directoryDateTimestamp']._text,
+      Country: {
+        countryNames: parsed['ns3:DirectoryRes']['ns3:Directory']['ns3:Country']['ns3:countryNames']._text,
+        Issuer: _parseIssuers(parsed['ns3:DirectoryRes']['ns3:Directory']['ns3:Country']['ns3:Issuer']),
+      },
+    },
+  }
+}
+
+interface IXMLNode {
+  _text: string
+}
+
+interface IXMLIssuer {
+  'ns3:issuerID': IXMLNode
+  'ns3:issuerName': IXMLNode
+}
+
+interface IIssuer {
+  issuerID: string
+  issuerName: string
+}
+
+function _parseIssuers(issuers: IXMLIssuer[]): IIssuer[] {
+  return issuers.map((issuer) => {
+    return {
+      issuerID: issuer['ns3:issuerID']._text,
+      issuerName: issuer['ns3:issuerName']._text,
+    }
+  })
 }
 
 export function fetchTransactionResponse(issuerID: string, transactionID: string, requestedService?: string) {
