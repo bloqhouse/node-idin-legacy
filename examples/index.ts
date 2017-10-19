75555310165b63281ea29750b79551908fe3ff45
phra <greensoncio@gmail.com>
2017-10-19 11:58:34 +0200

    fix(example): update example with till status response

--- examples/index.ts
+++ examples/index.ts
@@ -1,26 +1,57 @@
 // tslint:disable-next-line:no-reference
 /// <reference path="../untyped.d.ts" />
 
+import { ifError } from 'assert'
+import to from 'await-to-ts'
 import * as uuid from 'uuid/v1'
 
 import {
+  fetchStatusResponse,
   formatDirectoryProtocolXML,
   formatStatusProtocolXML,
   formatTransactionProtocolXML,
+  getDirectoryResponse,
+  getTransactionResponse,
 } from '../lib'
 
-const id = uuid().replace(/-/g, '')
+const isserID = 'BANKNL2Y'
+const acquirerID = '0030'
+const _ID = '000000000001'
+const transactionID = '0030000020088521'
+const merchantReference = `${acquirerID}${_ID}` // uuid().replace(/-/g, '')
 
 // tslint:disable-next-line:no-console
 console.log(`
-ID #${id}
+MerchantReference #${merchantReference}
+TxID #${transactionID}
 
 DIRECTORY REQUEST
 ${formatDirectoryProtocolXML()}
 
 TRANSACTION REQUEST
-${formatTransactionProtocolXML('BANKNL2Y', 'TXTID')}
+${formatTransactionProtocolXML('BANKNL2Y', merchantReference)}
 
 STATUS REQUEST
-${formatStatusProtocolXML(id)}
+${formatStatusProtocolXML(transactionID)}
 `)
+
+async function main() {
+  const [err0, directoryResponse] = await to(getDirectoryResponse())
+  ifError(err0)
+  // tslint:disable-next-line:no-console
+  console.log('Issuers', directoryResponse.Directory.Country.countryNames, directoryResponse.Directory.Country.Issuer)
+  const [err1, transactionResponse] = await to(getTransactionResponse(isserID, transactionID))
+  ifError(err1)
+  // tslint:disable-next-line:no-console
+  console.log(`
+  TransactionID: ${transactionResponse.Transaction.transactionID}
+  IssuerAuthenticationURL: ${transactionResponse.Issuer.issuerAuthenticationURL}
+  `)
+
+  const [err2, statusResponse] = await to(fetchStatusResponse(transactionResponse.Transaction.transactionID))
+  ifError(err2)
+  // tslint:disable-next-line:no-console
+  console.log('status xml response', statusResponse)
+}
+
+main()
